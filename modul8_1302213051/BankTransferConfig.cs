7551b3d2ab9e061e22fdbaff41e5577ec325de9e
Hasnannn <jannatinnurrohmah@gmail.com>
2023-04-10 08:07:37 +0700

    Bank transfer dengan configuration

---
+++ modul8_1302213051/BankTransferConfig.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul8_1302213051
+{
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set;}
+        public string methods { get; set; }
+        public string confirmation { get; set; }
+
+        public static BankTransferConfig LoadConfig()
+        {
+            BankTransferConfig config;
+            string configJSON = File.ReadAllText(@"D:\Kuliah\C#\modul8_1302213051\bank_transfer_config.json");
+            config = JsonConvert.DeserializeObject<BankTransferConfig>(configJSON);
+
+            return config;
+        }
+    }
+}
---
+++ modul8_1302213051/Program.cs
@@ -0,0 +1,24 @@
+﻿using modul8_1302213051;
+
+BankTransferConfig config = new BankTransferConfig();
+
+if(config.lang == "en")
+{
+    Console.WriteLine("Please insert the amount of money to transfer: \n");
+}else
+{
+    Console.WriteLine("Masukkan jumlah yang akan ditransfer: \n");
+}
+var input = int.Parse(Console.ReadLine());
+
+int total = 0;
+if (input == config.threshold)
+{
+    Console.WriteLine("Biaya transfer : " + config.low_fee);
+    //total = input + config.low_fee;
+}
+else if (input > config.threshold)
+{
+    Console.WriteLine("Biaya transfer : " + config.high_fee);
+    //total = input + config.high_fee;
+}
